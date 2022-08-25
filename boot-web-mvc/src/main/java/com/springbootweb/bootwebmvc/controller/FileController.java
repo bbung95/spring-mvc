1c63b88b7c156c56385d172b236ff6991407eb7e
bbung95 <tjd123123@naver.com>
2022-08-25 10:24:54 +0900

    Revert "feat : @ResponseBody FIle Download"
    This reverts commit 6d181137fc96043dd4dae85e96ac2f9526ab78fb.

--- boot-web-mvc/src/main/java/com/springbootweb/bootwebmvc/controller/FileController.java
+++ boot-web-mvc/src/main/java/com/springbootweb/bootwebmvc/controller/FileController.java
@@ -1,29 +1,15 @@
 package com.springbootweb.bootwebmvc.controller;
 
-import org.apache.tika.Tika;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
-
 @Controller
 public class FileController {
-
-    @Autowired
-    private ResourceLoader resourceLoader;
-
-    private final String PATH = "classpath:";
 
     @GetMapping("file")
     public String fileForm(Model model){
@@ -38,20 +24,5 @@ public class FileController {
         redirectAttributes.addFlashAttribute("message", message);
 
         return "redirect:/file";
-    }
-    
-    @GetMapping("file/{filename}")
-    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws Exception{
-
-        Resource resource = resourceLoader.getResource(PATH + filename);
-        File file = resource.getFile();
-
-        String mediaType = new Tika().detect(file);
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; file")
-                .header(HttpHeaders.CONTENT_TYPE, mediaType)
-                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
-                .body(resource);
     }
 }
